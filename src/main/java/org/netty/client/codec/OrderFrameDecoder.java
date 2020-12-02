fefa6567d8ab73fa229afb7d36047e8db24e0366
chenchunyong <815472970@qq.com>
2020-12-02 21:54:36 +0800

    编码方法

--- src/main/java/org/netty/client/codec/OrderFrameDecoder.java
+++ src/main/java/org/netty/client/codec/OrderFrameDecoder.java
@@ -4,6 +4,6 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
 public class OrderFrameDecoder extends LengthFieldBasedFrameDecoder {
     public OrderFrameDecoder() {
-        super(2, 0, 2, 0, 2);
+        super(Integer.MAX_VALUE, 0, 2, 0, 2);
     }
 }
